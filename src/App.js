203c3871701bd1dd0039b84842fd20f84a8d4704
taejung77 <132897696+taejung77@users.noreply.github.com>
2025-05-21 21:11:47 +0900

    회원가입 페이지

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { SignupProvider } from './context/SignupContext';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import AddressSearch from './pages/AddressSearch';
+
 
 // import logo from './logo.svg';
 import './App.css';
@@ -12,10 +15,14 @@ class App extends Component {
     return (
       <div className='App'>
         <Router>
+          <SignupProvider>
           <Routes>
             <Route path="/signup" element={<Signup />} />
             <Route path="/" element={<Login />} />
+            <Route path="/search-address" element={<AddressSearch />} />
+
           </Routes>
+          </SignupProvider>
         </Router>
       </div>
     );
---
+++ src/context/SignupContext.js
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from 'react';
+
+const SignupContext = createContext(null);
+
+export const SignupProvider = ({ children }) => {
+  const [form, setForm] = useState({
+    name: '',
+    phone: '',
+    age: '',
+    gender: '',
+    address: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  return (
+    <SignupContext.Provider value={{ form, setForm }}>
+      {children}
+    </SignupContext.Provider>
+  );
+};
+
+export const useSignup = () => {
+  const context = useContext(SignupContext);
+  if (!context) {
+    throw new Error('useSignup must be used within a SignupProvider');
+  }
+  return context;
+};
