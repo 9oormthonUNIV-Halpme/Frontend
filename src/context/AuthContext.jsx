3c2e15b3887e936838fcf7b3b2134b94fb842573
san-sae <choi.yoo.lim.12@gmail.com>
2025-05-30 05:24:22 +0900

    [FEAT] 채팅방 페이지

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -27,19 +27,26 @@ export const AuthProvider = ({ children }) => {
             },
         });
 
-        //console.log(res.data.data);
+        console.log(res.data.data);
 
         if (res.data.success){
             setUser(res.data.data);
-            console.log(res.data.data);
-            console.log("사용자 정보 가져오기 성공 후 저장")
+            // console.log(res.data.data);
+            console.log("사용자 정보 요청 성공")
         }
         else {
-            console.log("사용자 정보 가져오기 실패", res.data.message);
+            console.warn("사용자 정보 요청 실패(서버 응답 성공)", res.data.message);
+            logout(); 
         }
     }
     catch (err) {
-        console.error("사용자 정보 요청 실패", err);
+        if(err.response && err.response.status === 401){
+          console.warn("토큰이 만료되었거나 유효하지 않습니다. 자동으로 로그아웃 진행하였습니다.");
+          logout();
+        }
+        else{
+          console.error("네트워크 오류 또는 서버 오류", err);
+        }
     }
   }
 
